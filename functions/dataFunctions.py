bf2f806b8890f05161a0eb9443363c56c9ba0ed5
Paulo <44546537+happendo@users.noreply.github.com>
2024-06-10 00:10:20 -0300

    Update dataFunctions.py

--- functions/dataFunctions.py
+++ functions/dataFunctions.py
@@ -64,7 +64,7 @@ def getLocator(data):
         geolocator = Nominatim(user_agent='myapplication')
         location = geolocator.geocode(i, timeout=None)
         coords[0].append(location.latitude); coords[1].append(location.longitude)
-        time.sleep(1.0)
+        time.sleep(0.3)
     locData = pd.DataFrame({"Cidades": data, "latitude": coords[0], "longitude": coords[1], "size": data.count()})
     return locData
 
